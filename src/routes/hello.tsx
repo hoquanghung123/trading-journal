b3e3f14be07b9231749da6a49dddc34c8a676d9a
bsleducduy123 <bsleducduy@gmail.com>
2026-05-01 22:53:13 +0700

    chore: trigger redeploy after env vars update

--- src/routes/hello.tsx
+++ src/routes/hello.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+// Fresh build trigger after environment variable update
 export const Route = createFileRoute("/hello")({
   component: () => <div>Hello World</div>,
 });
